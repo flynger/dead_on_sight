76d74e82c3ca3795d7dd99f80b6114e0c55c77ca
Arnav Singh <arnavsingh2516@gmail.com>
2022-03-25 14:52:06 -0700

    stuff

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -8,29 +8,15 @@ namespace StarterAssets
     {
         public bool canUseHack = true;
         public GameObject target;
-/*
-        public int hitPoints
-        {
-            get;
-            set;
-        }
-
-        public int baseDamage
-        {
-            get;
-        }
-
-        public bool isHackable
-        {
-            get;
-        }*/
+        public Inventory inventory;
+        public GameObject pickupRange;
+        public GameObject[] itemsWithinRange;
 
         void Start()
         {
-            
+
         }
 
-        // Update is called once per frame
         void Update()
         {
             HackCheck();
@@ -38,29 +24,20 @@ namespace StarterAssets
 
         public void HackCheck()
         {
-            // Debug.DrawRay(transform.position + new Vector3(0.06f, 1.6f, 0f), transform.TransformDirection(Vector3.forward) * 16, Color.green);
-            // if (canUseHack && controller._input.possess)
-            // {
-            //     RaycastHit hit;
-            //     //ToggleCollider(false);
-            //     if (Physics.Raycast(transform.position + new Vector3(0.06f, 1.6f, 0f), transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, gameManager.entityLayer))
-            //     {
-            //         Debug.Log(hit.collider.gameObject);
-            //         Hack(hit.collider.gameObject);
-            //         StartCoroutine(HackCooldown());
-            //     }
-            //     else
-            //     {
-            //         //Debug.Log("hack fail");
-            //     }
-            //     //ToggleCollider(true);
-            // }
-            GameObject obj;
-            if (canUseHack && controller._input.possess && CheckVision(16, out obj) && obj != null && obj.CompareTag("enemy"))
+            if (CheckVision(16, out target) && target != null)
             {
-                Hack(obj);
-                StartCoroutine(HackCooldown());
+                if (target.CompareTag("enemy") && controller._input.possess && canUseHack)
+                {
+                    Hack(target);
+                    StartCoroutine(HackCooldown());
+                }
+                // else if (target.CompareTag("item") && controller._input.interact)
+                // {
+                //     inventory.DropItem();
+                //     inventory.item = target;
+                // }
             }
+            else target = null;
         }
 
         IEnumerator HackCooldown()
