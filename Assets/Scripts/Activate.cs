a142018e175ea6548e0f819f7f78e5311e0e7a29
Arnav Singh <arnavsingh2516@gmail.com>
2022-05-26 13:40:46 -0700

    ss

--- Assets/Scripts/Activate.cs
+++ Assets/Scripts/Activate.cs
@@ -2,51 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Activate : MonoBehaviour
+namespace StarterAssets
 {
-    public GameObject[] objects;
-    public GameObject activeSprite;
-    public GameObject cooldownSprite;
-    public float cooldownSec;
-    public string effect;
-    public bool isOneTimeUse;
-    public bool touchActivated;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class Activate : MonoBehaviour
     {
+        public GameObject[] objects;
+        public GameObject activeSprite;
+        public GameObject cooldownSprite;
+        public float cooldownSec;
+        public string effect;
+        public bool isOneTimeUse;
+        public bool touchActivated;
         
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
 
-    public void activateEffect() {
-        if (effect == "toggle") {
-            foreach (GameObject obj in objects) {
-                obj.SetActive(!obj.activeSelf);
+        public void activateEffect() {
+            if (effect == "toggle") {
+                foreach (GameObject obj in objects) {
+                    obj.SetActive(!obj.activeSelf);
+                }
+            }
+            else if (effect == "kill") {
+                GameObject.Find("Game Manager").GetComponent<GameManager>().player.GetComponent<Terminal>().ApplyDamage(20);
+            }
+            StartCoroutine(offOnCooldown());
+            if (isOneTimeUse) {
+                GetComponent<Activate>().enabled = false;
             }
         }
-        StartCoroutine(offOnCooldown());
-        if (isOneTimeUse) {
-            GetComponent<Activate>().enabled = false;
+
+        IEnumerator offOnCooldown() {
+            cooldownSprite.SetActive(true);
+            activeSprite.SetActive(false);
+            yield return new WaitForSeconds(cooldownSec);
+            cooldownSprite.SetActive(false);
+            activeSprite.SetActive(true);
         }
-    }
 
-    IEnumerator offOnCooldown() {
-        cooldownSprite.SetActive(true);
-        activeSprite.SetActive(false);
-        yield return new WaitForSeconds(cooldownSec);
-        cooldownSprite.SetActive(false);
-        activeSprite.SetActive(true);
-    }
-
-    void OnTriggerEnter(Collider other) {
-        if (!other.CompareTag("enemy") && touchActivated) {
-            activateEffect();
+        void OnTriggerEnter(Collider other) {
+            if (!other.CompareTag("enemy") && touchActivated) {
+                activateEffect();
+            }
         }
     }
 }
